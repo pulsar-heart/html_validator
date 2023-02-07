83ed7ededada1ca897068c7daed475d38fdd4065
Kaia Smith <coniferkai@gmail.com>
2023-02-06 19:37:37 -0800

    Should pass doctest, adding HTML Validator

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -16,6 +16,26 @@ def validate_html(html):
     # then process these html tags using the balanced parentheses algorithm from the class/book
     # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
+    balanced = True
+    index = 0
+    tags = _extract_tags(html)
+    s = []
+    while index < len(tags) and balanced:
+        tag = tags[index]
+        if not '</' in tag:
+            s.append(tag)
+        else:
+            if len(s) == 0:
+                balanced = False
+            else: 
+                top = s.pop()
+                top_word = top[1:-1]
+                this_word = tag[2:-1]
+                if not top_word == this_word:
+                    balanced = False
+        index += 1
+    bool1 = balanced and len(s) == 0
+    return bool1
 
 
 def _extract_tags(html):
@@ -29,3 +49,17 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    is_tag = False
+    tags = []
+    this_tag = ''
+    for symbol in html:
+        if symbol == '<':
+            is_tag = True
+            this_tag += '<'
+        elif is_tag:
+            this_tag += symbol
+            if symbol == '>':
+                is_tag = False
+                tags.append(this_tag)
+                this_tag = ''
+    return tags
